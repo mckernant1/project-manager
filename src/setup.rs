fae1499c919e4d54bad93cc7e9bab0359cc4601e
Tom McKernan <tmeaglei@gmail.com>
2019-08-26 20:42:14 -0400

    pull and list

--- src/setup.rs
+++ src/setup.rs
@@ -4,39 +4,76 @@ use std::fs::File;
 use std::io::{Read, stdout, Write, stdin};
 use std::path::PathBuf;
 
+#[derive(Clone)]
 pub struct SettingsFile {
-    settings_file: File,
+    settings_file_path: String,
 }
 
 impl SettingsFile {
     pub fn new() -> SettingsFile {
         let settings_file_path = format!("{}/.gg.json", env::var("HOME").unwrap());
 
-        let  settings_file_res = File::open(settings_file_path.clone());
+        let settings_file_res = File::open(settings_file_path.clone());
 
         let mut settings_file_mut = match settings_file_res {
             Ok(t) => t,
-            Err(_) => create_settings_file(settings_file_path)
+            Err(_) => create_settings_file(settings_file_path.clone())
         };
 
+        let settings_json = get_settings_json(&mut settings_file_mut);
+
         return SettingsFile {
-            settings_file: settings_file_mut
+            settings_file_path: settings_file_path.clone(),
         };
     }
 
-    pub fn get_settings_json(mut self) -> JsonValue {
-        let mut settings_string = String::new();
-        self.settings_file.read_to_string(&mut settings_string).unwrap();
+    pub fn add_repo(self, repo_path: &str, repo_name: &str) {
+        let repos = get_settings_json(
+            &mut File::open(
+                self.settings_file_path.clone()
+            ).unwrap()
+        );
+        let repo = object! {
+            "name" => repo_name,
+            "path" => repo_path
+        };
 
-        let settings_json = json::parse(settings_string.as_str()).unwrap();
-        return settings_json;
+        let mut repos_mut = repos.clone();
+
+        if !repos_mut.contains("repos") {
+            repos_mut["repos"] = array![];
+        }
+
+        repos_mut["repos"].push(repo).unwrap();
+
+        let settings_string = json::stringify(repos_mut);
+
+        fs::write(
+            self.settings_file_path.clone(),
+            settings_string.as_bytes(),
+        ).unwrap();
+    }
+
+    pub fn list_repos(self) -> JsonValue {
+        let repos =
+            get_settings_json(
+                &mut File::open(self.settings_file_path)
+                    .unwrap()
+            )["repos"].clone();
+
+        return repos;
+    }
+
+    pub fn get_settings_json(self) -> JsonValue {
+        let mut file = File::open(self.settings_file_path).unwrap();
+        get_settings_json(&mut file)
     }
 }
 
 fn create_settings_file(settings_file_path: String) -> File {
     let mut home_dir = String::new();
     print!("Input the path of where you clone your repos (default is $HOME/Desktop): ");
-    stdout().flush();
+    stdout().flush().unwrap();
     stdin().read_line(&mut home_dir).unwrap();
 
     if home_dir == "\n" {
@@ -51,4 +88,13 @@ fn create_settings_file(settings_file_path: String) -> File {
     fs::write(settings_file_path.clone(), json_string).unwrap();
 
     return File::open(settings_file_path.clone()).unwrap();
+}
+
+fn get_settings_json(settings_file: &mut File) -> JsonValue {
+    let mut settings_string = String::new();
+    settings_file.read_to_string(&mut settings_string).unwrap();
+
+    let settings_json =
+        json::parse(settings_string.as_str()).unwrap();
+    return settings_json;
 }
--- src/subcommands.rs
+++ src/subcommands.rs
@@ -1,41 +1,76 @@
 use clap::ArgMatches;
 use json::JsonValue;
-use std::process::Command;
+use std::process::{Command, Stdio};
 use crate::setup::SettingsFile;
 
 pub fn clone(matches: ArgMatches, settings_file: SettingsFile) {
-
     let repo_string = matches
         .subcommand_matches("clone").unwrap()
         .value_of("LINK").unwrap().to_string().clone();
 
-    let json = settings_file.get_settings_json();
-    let default_dir = json["defaultDir"].as_str().unwrap().clone();
+    let json_clone = settings_file
+        .clone().get_settings_json().clone();
+    let default_dir = json_clone["defaultDir"].as_str().unwrap().clone();
     let repo_name: String = repo_string.split('/').last().unwrap()
-        .chars().take_while(|c| { c != &'.'}).collect();
+        .chars().take_while(|c| { c != &'.' }).collect();
     let repo_dir = format!("{}/{}", default_dir, repo_name);
-    println!("cloning {} to {}", repo_string, repo_dir);
+    println!("Cloning {} to {}\n", repo_string, repo_dir);
 
-
-    let thread = Command::new("git")
+    let mut child = Command::new("git")
         .arg("clone")
         .arg(repo_string)
-        .arg(repo_dir)
+        .arg(repo_dir.clone())
         .spawn().unwrap();
+    child.wait().unwrap();
+    settings_file.clone().add_repo(
+        repo_dir.clone().as_str(),
+        repo_name.clone().as_str(),
+    );
 }
 
 pub fn pull(matches: ArgMatches, settings_file: SettingsFile) {
+    let repos = settings_file.list_repos();
 
+    if matches.subcommand_matches("pull").unwrap()
+        .is_present("NAME") {
+        let repo_name = matches
+            .subcommand_matches("pull").unwrap()
+            .value_of("NAME").unwrap();
+        let path = repos[repo_name]["path"].to_string();
+        println!("{}; {}", repo_name, path);
+        Command::new("git")
+            .arg("-C")
+            .arg(path)
+            .arg("pull")
+            .spawn().unwrap()
+            .wait().unwrap();
+    } else {
+        let mut child_list: Vec<_> = vec![];
+        for member in repos.members() {
+            println!("Pulling: {}", member["name"].to_string());
+            child_list.push(
+                Command::new("git")
+                    .arg("-C")
+                    .arg(member["path"].to_string())
+                    .arg("pull")
+                    .stdout(Stdio::null())
+                    .spawn().unwrap()
+            )
+        }
+        for mut child in child_list {
+           child.wait().unwrap();
+        }
+    }
+    println!();
 }
 
 pub fn list(matches: ArgMatches, settings_file: SettingsFile) {
-
+    let repos = settings_file.list_repos();
+    for member in repos.members() {
+        println!("{}", member["name"].to_string());
+    }
 }
 
-pub fn add(matches: ArgMatches, settings_file: SettingsFile) {
+pub fn add(matches: ArgMatches, settings_file: SettingsFile) {}
 
-}
-
-pub fn delete(matches: ArgMatches, settings_file: SettingsFile) {
-
-}
+pub fn delete(matches: ArgMatches, settings_file: SettingsFile) {}
